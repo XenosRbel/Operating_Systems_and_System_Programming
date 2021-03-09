56ed1ca56976b16414674867bc6e559bd3327976
XenosRbel <delldd@yandex.by>
2021-03-10 00:04:41 +0300

    Добавить проверку параметров для лабораторной работы 2.

---
+++ Laba_5/L_2_v_1/CrypteFile.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace L_2_v_1
+{
+	internal class CrypteFile
+	{
+		private readonly Aes _aes;
+
+		private readonly string _folderPath;
+		private readonly byte _coreAvailable;
+		private readonly byte _environmentProcessorCount;
+		private object _lockObject;
+		
+		public byte NumberBusyThread { get; set; }
+
+		public CrypteFile(string folderPath, byte coreAvailable, byte environmentProcessorCount)
+		{
+			_folderPath = folderPath ?? throw new ArgumentNullException(nameof(folderPath));
+			_coreAvailable = coreAvailable;
+			_environmentProcessorCount = environmentProcessorCount;
+
+			_aes = Aes.Create();
+
+			_aes.GenerateKey();
+			var key = _aes.Key;
+			SaveKey(key);
+
+			_aes.GenerateIV();
+			var iv = _aes.IV;
+			SaveInitializationVector(iv);
+		}
+
+		private void Crypt(string filePath)
+		{
+			using var fs = new FileStream(filePath, FileMode.Open);
+	  
+			using var cryptStream = new CryptoStream(
+				fs,
+				_aes.CreateEncryptor(),
+				CryptoStreamMode.Write);
+
+			cryptStream.Dispose();
+		}
+
+		private void CryptFilesInFolder()
+		{
+			lock (_lockObject)
+			{
+
+			}
+		}
+
+		private void SaveInitializationVector(byte[] iv)
+		{
+			Save(iv, "iv.key");
+		}
+
+		private void SaveKey(byte[] key)
+		{
+			Save(key, "aes.key");
+		}
+
+		private void Save(byte[] data, string file)
+		{
+			using FileStream myStream = new FileStream(file, FileMode.OpenOrCreate);
+			myStream.Write(data, 0, data.Length);
+			myStream.Close();
+			myStream.Dispose();
+		}
+
+		public void PrintThreadStatus()
+		{
+			var threadId = int.MaxValue;
+			var filePath = string.Empty;
+			var cryptedBytes = int.MaxValue;
+
+			Console.WriteLine($"Thread ID:{threadId}\t" +
+				$"Full file path:{filePath}\tCommon count crypted bytes: {cryptedBytes}");
+		}
+	}
+}
---
+++ Laba_5/L_2_v_1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace L_2_v_1
+{
+	class Program
+	{
+		private static string _folderPath;
+		private static byte _coreAvailable;
+		private static byte _environmentProcessorCount;
+
+		static void Main(string[] args)
+		{
+			try
+			{
+				_environmentProcessorCount = (byte)Environment.ProcessorCount;
+				Initialization(args);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine($"Ошибка ввода аргументов программы.\n{e.Message}");
+			}
+		}
+
+		private static void Initialization(string[] args)
+		{
+			if (IsValidProgramArgument(args))
+			{
+				CryptFiles();
+			}
+			else
+			{
+				Console.Write("Введите путь к папке для шифрования:");
+				_folderPath = Console.ReadLine();
+
+				Console.Write($"Доступно потоков:{Environment.ProcessorCount}\n" +
+					$"Введите число одновременно работающих потоков для шифрования файлов:");
+				_coreAvailable = Convert.ToByte(Console.ReadLine());
+
+				ValidateManualParams();
+
+				CryptFiles();
+			}
+		}
+
+		private static void CryptFiles()
+		{
+			var crypto = new CrypteFile(_folderPath, _coreAvailable, _environmentProcessorCount);
+		}
+
+		private static bool IsValidProgramArgument(string[] args)
+		{
+			return args.Length == 2;
+		}
+
+		private static void ValidateManualParams()
+		{
+			var invalidParams = new List<string>();
+
+			if (_coreAvailable <= 0 || _coreAvailable > Environment.ProcessorCount)
+			{
+				invalidParams.Add(nameof(_coreAvailable));
+			}
+
+			if (!Directory.Exists(_folderPath))
+			{
+				invalidParams.Add(nameof(_folderPath));
+			}
+
+			if (invalidParams.Count > 0)
+			{
+				throw new ArgumentNullException(string.Join(",", invalidParams),"Неправильные аргументы приложения.");
+			}
+		}
+	}
+}
